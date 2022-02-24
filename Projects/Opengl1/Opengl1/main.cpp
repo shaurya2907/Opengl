7d38c9cf36caa83aded0d05a59b7aa8db5246e1f
shaurya <sharmashaurya2907@gmail.com>
2022-02-25 01:45:39 +0530

    projection

--- Projects/Opengl1/Opengl1/main.cpp
+++ Projects/Opengl1/Opengl1/main.cpp
@@ -14,7 +14,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO,IBO, shader, uniformModel;
+GLuint VAO, VBO,IBO, shader, uniformModel , uniformProjection;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -41,10 +41,11 @@ layout(location = 0) in vec3 pos;          \n\
 out vec4 vCol;								 \n\
                                               \n\
  uniform mat4 model;                           \n\
+uniform mat4 projection;                           \n\
                                                 \n\
 void main()                                      \n\
 {                                                 \n\
-	gl_Position = model*vec4( pos, 1.0);		   \n\
+	gl_Position = projection*model*vec4( pos, 1.0);		   \n\
 	vCol = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);		\n\
  }";
 
@@ -166,6 +167,7 @@ if (!result) {
 
 uniformModel = glGetUniformLocation(shader, "model");
 
+uniformProjection = glGetUniformLocation(shader, "projection");
 
 
 }
@@ -227,6 +229,7 @@ int main()
 	CreateTriangle();
 	CompileShaders();
 
+	glm::mat4 projection = glm::perspective(45.0f, (GLfloat)bufferWidth / (GLfloat)bufferHeight, 0.1f, 100.0f);
 
 
 	// Loop until window closed
@@ -277,17 +280,15 @@ int main()
 			glUseProgram(shader);
 
 			glm::mat4 model(1.0f);
-			
+			model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
 			model = glm::rotate(model, curAngle*toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
-			//model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
-			
 			model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 			
 
 
 
 			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-
+			glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
 			glBindVertexArray(VAO);
 			
